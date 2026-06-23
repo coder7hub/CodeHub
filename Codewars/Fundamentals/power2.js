b5d13f42227919abb0943ebfebb297582bf6053b
Coder7hub <j7hubil@gmail.com>
2026-06-23 20:11:37 +0400

    Power 2

---
+++ Codewars/Fundamentals/power2.js
@@ -0,0 +1,20 @@
+let n = 3
+
+function powersOfTwo(n){ 
+let answer=[]
+
+ for (let i = 0; i <= n; i++) {
+    answer.push(2 ** i);
+  }
+
+return answer
+}
+console.log(powersOfTwo(n))
+
+
+
+
+
+
+
+
